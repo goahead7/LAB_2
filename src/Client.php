9d80aa2abbc30e32c9a788ddb99ad02a4febd722
Sophia <sophia.kuznetsova.7@gmail.com>
2022-04-04 14:28:38 +1000

    add file storage

--- src/Client.php
+++ src/Client.php
@@ -2,169 +2,204 @@
 
 namespace App\Class;
 
-class Client {
-    private string $url;/**
- * Client constructor.
- * @param string $url
- */public function __construct ($url)
-   {
-    $this->url = $url;
-   }
+class Client
+{
+    private string $url;
 
-    public function registration($login, $password)
+    /**
+     * Client constructor.
+     * @param string $url
+     */
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+    public function registration(string $login, string $password)
     {
         $data = json_encode(['login' => $login,
                              'password' => $password],
-                             JSON_UNESCAPED_UNICODE);
-        
-        $request = curl_init();
-        
-        $arr = array(CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_URL => $this->baseURL.'/user',
+                        JSON_UNESCAPED_UNICODE);
+        $req = curl_init($this->url . '/user');
+        $arr = array(
+            CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => array(
                 'Content-type: application/json',
-                'Content-Length: '.strlen($data)),
+                'Content-Length: ' . strlen($data)),
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => $data);
 
-        curl_setopt_array($request, $arr);
-        
-        try
-        {
-            $data = curl_exec($request);
-            $result = curl_getinfo($request, CURLINFO_RESPONSE_CODE);
-        }
-        finally
-        {
-            curl_close($request);
-        }
-        return $result;
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
 
+        curl_close($req);
+
+        return $response;
     }
 
-    public function create_todo($login, $todo) 
+    public function create_todo(string $discription, string $todo_name, int $id)
     {
-        $data = json_encode(['login' => $login,
-                             'todo'  => $todo],
-                             JSON_UNESCAPED_UNICODE);
-        $request = curl_init();
-        
+        $data = json_encode(['discription' => $discription,
+                             'todo_name' => $todo_name],
+                       JSON_UNESCAPED_UNICODE);
+        $req = curl_init($this->url . '/todo/' . $id);
         $arr = array(
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_URL => $this->url.'/todo',
             CURLOPT_HTTPHEADER => array(
                 'Content-type: application/json',
-                'Content-Length'.strlen($data)),
+                'Content-Length' . strlen($data)),
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $data );
-        
-        curl_setopt_array($request, $arr);
-        
-        try
-        {
-            $data = curl_exec($request);
-            $result = curl_getinfo($request, CURLINFO_RESPONSE_CODE);
-        }
-        finally 
-        {
-            curl_close($request);
-        }
-        return $result;
+            CURLOPT_POSTFIELDS => $data);
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
     }
 
-    public function get_todo($login) 
-    {    
-        $data = json_encode(['login' => $login],
-                             JSON_UNESCAPED_UNICODE);
-        
-        $request = curl_init();
-        
+    public function get_todo(int $id)
+    {
+        $data = json_encode(['id' => $id],
+                       JSON_UNESCAPED_UNICODE);
+        $req = curl_init($this->url . '/todo/' . $id);
         $arr = array(
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_URL => $this->url.'/todo',
             CURLOPT_HTTPHEADER => array(
                 'Content-type: application/json',
-                'Content-Length'.strlen($data)),
+                'Content-Length' . strlen($data)),
             CURLOPT_CUSTOMREQUEST => 'GET',
             CURLOPT_POSTFIELDS => $data);
-        
-        curl_setopt_array($request, $arr);
-        
-        try
-        {
-            $data = curl_exec($request);
-            $result = curl_getinfo($request, CURLINFO_RESPONSE_CODE);
-        }
-        finally 
-        {
-            curl_close($request);
-        }
-        return $result;
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
     }
 
-    public function put_todo($id, $login, $todo) 
+    public function put_todo(int $id, string $discription, string $todo_name)
     {
-        $data = json_encode(['id' => $id,
-                             'login' => $login,
-                             'todo' => $todo],
-                             JSON_UNESCAPED_UNICODE);
-        
-        $request = curl_init();
-        
+        $data = json_encode(['discription' => $discription,
+                             'todo_name' => $todo_name],
+                       JSON_UNESCAPED_UNICODE);
+        $req = curl_init($this->url . '/todo/' . $id);
         $arr = array(
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_URL => $this->url.'/todo'.$id,
             CURLOPT_HTTPHEADER => array(
                 'Content-type: application/json',
-                'Content-Length'.strlen($data)),
+                'Content-Length' . strlen($data)),
             CURLOPT_CUSTOMREQUEST => 'PUT',
             CURLOPT_POSTFIELDS => $data);
-        
-        curl_setopt_array($request, $arr);
-        
-        try
-        {
-            $data = curl_exec($request);
-            $result = curl_getinfo($request, CURLINFO_RESPONSE_CODE);
-        }
-        finally 
-        {
-            curl_close($request);
-        }
-        return $result;
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
     }
 
-    public function delete_todo($id, $login, $todo) 
+    public function delete_todo(int $id)
     {
-         $data = json_encode(['id' => $id,
-                             'login' => $login,
-                             'todo' => $todo],
-                              JSON_UNESCAPED_UNICODE);
-        
-        $request = curl_init();
-        
+        $data = json_encode(['id' => $id],
+                      JSON_UNESCAPED_UNICODE);
+        $req = curl_init($this->url . '/todo/' . $id);
         $arr = array(
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_URL => $this->url.'/todo'.$id,
             CURLOPT_HTTPHEADER => array(
                 'Content-type: application/json',
-                'Content-Length'.strlen($data)),
+                'Content-Length' . strlen($data)),
             CURLOPT_CUSTOMREQUEST => 'DELETE',
-            CURLOPT_POSTFIELDS => $data );
-        
-        curl_setopt_array($request, $arr);
-        
-        try
-        {
-            $data = curl_exec($request);
-            $result = curl_getinfo($request, CURLINFO_RESPONSE_CODE);
-        }
-        finally
-        {
-            curl_close($request);
-        }
-        return $result;
+            CURLOPT_POSTFIELDS => $data);
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
+    }
+//file
+
+    public function add_file(string $file)
+    {
+        $req = curl_init($this->url . '/files/add');
+        $arr = array(CURLOPT_RETURNTRANSFER=>true,
+                     CURLOPT_CUSTOMREQUEST=>'POST',
+                     CURLOPT_POSTFIELDS => array('file' => new CurlFile($file)));
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
+    }
+
+    public function delete_file( string $f_name)
+    {
+        $req = curl_init($this->url . '/files/' . $f_name);
+        $arr = array(CURLOPT_RETURNTRANSFER=>true,
+                     CURLOPT_CUSTOMREQUEST=>'DELETE');
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
+    }
+
+    public function get_files()
+    {
+        $req = curl_init($this->url . '/files/get_files');
+        $arr = array(CURLOPT_RETURNTRANSFER=>true,
+                     CURLOPT_CUSTOMREQUEST=>'GET');
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
+    }
+
+    public function download_file($f_name)
+    {
+        $req = curl_init($this->url . '/files/' . $f_name);
+        $arr = array(CURLOPT_RETURNTRANSFER=>true,
+                     CURLOPT_CUSTOMREQUEST=>'GET');
+
+        curl_setopt_array($req, $arr);
+        $response = curl_exec($req);
+
+        curl_close($req);
+
+        return $response;
+
     }
 
 }
+
+
 
